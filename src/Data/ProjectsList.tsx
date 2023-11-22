5e9e21339535b543424df716ea4d3903123b9dea
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-23 08:52:58 +0900

    Modify to use TechnologiesList

--- src/Data/ProjectsList.tsx
+++ src/Data/ProjectsList.tsx
@@ -1,12 +1,18 @@
 import gramCopyImg from "../assets/GramCopy.png";
 import wildGooseChase from "../assets/WildGooseChase.png";
 import photoTag from "../assets/PhotoTag.png";
+import technologiesList from "./TechnologiesList";
+
+type Technology = {
+  name: string;
+  icon: JSX.Element;
+};
 
 export type Project = {
   name: string;
   image: string;
   description: string;
-  technologies: string[];
+  technologies: Technology[];
   siteLink: string;
   githubLink: string;
 };
@@ -16,7 +22,14 @@ const projectsList: Project[] = [
     name: "GramCopy",
     image: gramCopyImg,
     description: "Full-stack Instagram-like social media application",
-    technologies: ["React", "Sass", "MongoDB", "Express.JS", "Tanstack Query"],
+    technologies: [
+      technologiesList.javascript,
+      technologiesList.react,
+      technologiesList.sass,
+      technologiesList.mongodb,
+      technologiesList.expressjs,
+      technologiesList.tanstack,
+    ],
     siteLink: "https://gram-copy.vercel.app/",
     githubLink: "https://github.com/Stillwell-C/gram-copy",
   },
@@ -25,12 +38,13 @@ const projectsList: Project[] = [
     image: wildGooseChase,
     description: "Full stack blogging application.",
     technologies: [
-      "React",
-      "Sass",
-      "MongoDB",
-      "Express.JS",
-      "Redux/RTK Query",
-      "Supertest",
+      technologiesList.javascript,
+      technologiesList.react,
+      technologiesList.sass,
+      technologiesList.mongodb,
+      technologiesList.expressjs,
+      technologiesList.redux,
+      { name: "Supertest", icon: null },
     ],
     siteLink: "https://wild-goose-chase.vercel.app/",
     githubLink: "https://github.com/Stillwell-C/blog-frontend",
@@ -40,12 +54,12 @@ const projectsList: Project[] = [
     image: photoTag,
     description: "Full stack photo tagging game application.",
     technologies: [
-      "Typescript",
-      "React",
-      "TailwindCSS",
-      "PostgreSQL",
-      "Express.JS",
-      "Jest",
+      technologiesList.typescript,
+      technologiesList.react,
+      technologiesList.tailwind,
+      technologiesList.postgresql,
+      technologiesList.expressjs,
+      technologiesList.jest,
     ],
     siteLink: "https://photo-tag-sql.vercel.app/",
     githubLink: "https://github.com/Stillwell-C/photoTag/tree/postgreSQL",
