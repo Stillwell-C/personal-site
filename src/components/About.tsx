b2d594e1641c8896739721dfe03003e04f0ab1d0
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-23 08:53:27 +0900

    Add salutation div

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const About = () => {
-  return <div className='min-h-screen'>About</div>;
+  return (
+    <div className='h-screen flex'>
+      <div className='h-full w-full flex items-center justify-center'>
+        <h2 className='text-6xl'>Howdy.</h2>
+      </div>
+    </div>
+  );
 };
 
 export default About;
