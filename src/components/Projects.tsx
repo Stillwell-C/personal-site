6de687d7172697c8f6f1f4260cf97e318850ae98
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-22 15:35:40 +0900

    Add animation

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,27 +1,25 @@
-import { useScroll } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 import projectsList from "../Data/ProjectsList";
 import SingleProject from "./SingleProject";
 import { useRef } from "react";
 
 const Projects = () => {
-  const sectionref = useRef<HTMLElement>(null);
+  const sectionref = useRef<HTMLDivElement>(null);
 
   const { scrollYProgress } = useScroll({
     target: sectionref,
-    offset: ["start start", "end start"],
+    offset: ["start end", "start start"],
   });
 
+  console.log(scrollYProgress.get());
+
   return (
-    <section
-      ref={sectionref}
-      className='relative overflow-x-hidden'
-      id='projects'
-    >
+    <section className='relative overflow-x-hidden' id='projects'>
       <div>
         <h2 className='sticky top-0 pt-20 mx-20 mb-40 text-left text-4xl'>
           Projects
         </h2>
-        <div className='flex flex-col gap-60'>
+        <div ref={sectionref} className='flex flex-col gap-60'>
           {projectsList.map((project) => (
             <SingleProject
               scrollYProgress={scrollYProgress}
