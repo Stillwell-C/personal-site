03b6db2a70928c242c1c3448965fa495aeee2104
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-25 11:53:28 +0900

    Add second intersection observer

--- src/App.tsx
+++ src/App.tsx
@@ -8,33 +8,49 @@ import Contact from "./components/Contact";
 
 function App() {
   const [collapseNav, setCollapseNav] = useState<boolean>(false);
+  const [navColorShift, setNavColorShift] = useState<boolean>(false);
 
-  const scrollRefOptions = {
+  const collapseNavOptions = {
     rootMargin: "-150px 0px 0px 0px",
+  };
+  const navColorShiftOptions = {
+    rootMargin: "0px 0px 0px 0px",
   };
 
   const scrollObserver = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    const ScrollRefCheck = new IntersectionObserver((entries) => {
+    const collapseNavCheck = new IntersectionObserver((entries) => {
       const entry = entries[0];
       if (!entry.isIntersecting) {
-        console.log("not intersecting");
         setCollapseNav(true);
       }
       if (entry.isIntersecting) {
-        console.log("intersecting");
         setCollapseNav(false);
       }
-    }, scrollRefOptions);
-    if (scrollObserver.current) ScrollRefCheck.observe(scrollObserver.current);
+    }, collapseNavOptions);
+
+    const navColorShiftCheck = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      if (!entry.isIntersecting) {
+        setNavColorShift(true);
+      }
+      if (entry.isIntersecting) {
+        setNavColorShift(false);
+      }
+    }, navColorShiftOptions);
+
+    if (scrollObserver.current) {
+      collapseNavCheck.observe(scrollObserver.current);
+      navColorShiftCheck.observe(scrollObserver.current);
+    }
   }, [scrollObserver?.current]);
 
   useEffect(() => console.log("state", collapseNav), [collapseNav]);
 
   return (
     <div className='bg-white-smoke'>
-      <Header collapseNav={collapseNav} />
+      <Header collapseNav={collapseNav} navColorShift={navColorShift} />
       <main>
         <Hero ref={scrollObserver} />
         <About />
