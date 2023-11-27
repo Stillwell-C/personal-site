5db93dff51c83c4ed8ab463b1a113f6aec3d74dd
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-27 11:37:30 +0900

    Change button color & size

--- src/components/MobileNavHamburgerBtn.tsx
+++ src/components/MobileNavHamburgerBtn.tsx
@@ -13,12 +13,12 @@ const MobileNavHamburgerBtn = ({ open, setOpen, navColorShift }: PropType) => {
       onClick={() => setOpen((prev: boolean) => !prev)}
     >
       <div
-        className={`rounded h-1 w-10 top-4 absolute -mt-0.5 transition-all duration-700 before:content-[''] before:w-10 before:h-1 before:rounded before:absolute before:duration-700 before:transition-all before:-translate-x-5  after:w-10 after:rounded after:h-1 after:absolute after:duration-700 after:transition-all after:-translate-x-5 ${
+        className={`rounded h-1 w-7 top-4 absolute -mt-0.5 transition-all duration-700 before:content-[''] before:w-7 before:h-1 before:rounded before:absolute before:duration-700 before:transition-all before:-translate-x-3.5  after:w-7 after:rounded after:h-1 after:absolute after:duration-700 after:transition-all after:-translate-x-3.5 ${
           open
-            ? "bg-transparent before:translate-y-0 before:rotate-45 after:translate-y-0 after:-rotate-45 rotate-[360deg]"
+            ? "before:bg-white after:bg-white bg-transparent before:translate-y-0 before:rotate-45 after:translate-y-0 after:-rotate-45 rotate-[360deg]"
             : `${
                 navColorShift ? "bg-black" : "bg-white"
-              } before:-translate-y-3 after:translate-y-3`
+              } before:-translate-y-2.5 after:translate-y-2.5`
         } ${
           navColorShift
             ? "before:bg-black after:bg-black"
