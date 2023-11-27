92066d3cbc915c979f79dae7973369d5d1b74302
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-27 15:25:42 +0900

    Add responsive styles

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,32 +2,6 @@ import { motion } from "framer-motion";
 import React from "react";
 import { skillsList } from "../Data/SkillsData";
 
-// type SkillType = {
-//   name: string;
-//   iconUrl?: string;
-// };
-
-// const skillsList = [
-//   "Javascript",
-//   "Typescript",
-//   "HTML",
-//   "CSS",
-//   "SASS",
-//   "Tailwind CSS",
-//   "React",
-//   "Redux",
-//   "Tanstack Query",
-//   "Node.js",
-//   "Express.js",
-//   "MongoDB",
-//   "PostgreSQL",
-//   "Git",
-//   "Github",
-//   "Jest",
-//   "Webpack",
-//   "Linux",
-// ];
-
 const fadeInAnimationVariants = {
   initial: {
     opacity: 0,
@@ -46,11 +20,11 @@ const Skills = () => {
   return (
     <section
       id='skills'
-      className='min-h-screen flex justify-center items-center bg-[#f4f4f4] '
+      className='h-screen py-24 md:px-8 flex justify-center items-center bg-[#f4f4f4] '
     >
-      <div className='max-w-3xl py-24 border-2 border-l-0 border-r-0 '>
+      <div className='max-w-[90%] md:max-w-3xl md:py-24 md:border-2 md:border-l-0 md:border-r-0 '>
         <h3 className='text-center text-4xl mb-8'>Skills</h3>
-        <ul className='flex justify-center flex-wrap gap-4'>
+        <ul className='flex justify-center items-end flex-wrap gap-4'>
           {skillsList.map((skill, index) => {
             return (
               <motion.li
@@ -62,10 +36,10 @@ const Skills = () => {
                 custom={index}
                 className='p-4 text-center group'
               >
-                <div className='w-full flex items-center justify-center [&>svg]:max-w-48 [&>svg]:max-h-48 transition ease-in-out duration-200 group-hover:-translate-y-4'>
+                <div className='w-full flex items-center justify-center [&>svg]:max-w-[30px] [&>svg]:max-h-[30px] md:[&>svg]:max-w-48 md:[&>svg]:max-h-48 transition ease-in-out duration-200 group-hover:-translate-y-4'>
                   {skill.icon}
                 </div>
-                <div className='transition ease-in-out duration-200 group-hover:-translate-y-4'>
+                <div className='text-sm md:text-lg transition ease-in-out duration-200 group-hover:-translate-y-4'>
                   <span>{skill.name}</span>
                 </div>
               </motion.li>
