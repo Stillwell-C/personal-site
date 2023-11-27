f1e95a71423ba17158cf72ca83316db3a3d780dd
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-27 14:54:19 +0900

    Adjust margins, gaps, etc for responsiveness

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,14 +14,18 @@ const Projects = () => {
   console.log(scrollYProgress.get());
 
   return (
-    <section className='relative overflow-x-hidden mb-40 mt-10' id='projects'>
+    <section className='relative overflow-hidden mb-40 mt-10' id='projects'>
       <div>
         <h2 className=' mb-40 text-center text-4xl'>Projects</h2>
-        <div ref={sectionref} className='flex flex-col gap-20'>
-          {projectsList.map((project) => (
+        <div
+          ref={sectionref}
+          className='flex flex-col gap-20 sm:gap-60 md:gap-20 mb-48'
+        >
+          {projectsList.map((project, i) => (
             <SingleProject
               scrollYProgress={scrollYProgress}
               project={project}
+              index={i}
               key={project.name}
             />
           ))}
