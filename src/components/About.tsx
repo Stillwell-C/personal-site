3783eaf26fd284759649cab6dd57e1db3a061c97
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-27 17:54:29 +0900

    Change height

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className='h-screen flex'>
+    <div className='h-[150vh] flex'>
       <div className='h-full w-full flex items-center justify-center'>
         <h2 className='text-6xl'>Howdy.</h2>
       </div>
