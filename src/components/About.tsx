313e4ea8c259a5d33449b6701abe122c12a7c437
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-30 17:33:17 +0900

    Update responsive parallax animation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,8 +17,8 @@ const headerVariants = {
     y: 0,
     opacity: 1,
     transition: {
-      delay: 0.5,
-      duration: 0.5,
+      delay: 0.35,
+      duration: 0.35,
     },
   },
 };
@@ -30,7 +30,7 @@ const textVariants = {
   animate: {
     opacity: 1,
     transition: {
-      delay: 1.5,
+      delay: 1,
       duration: 0.8,
     },
   },
@@ -44,31 +44,47 @@ const About = ({ aboutParallaxScrollYProg }: PropsType) => {
   const ref = useRef<HTMLDivElement>(null);
   const headingRef = useRef<HTMLHeadingElement>(null);
 
-  // const { scrollYProgress } = useScroll({
-  //   target: ref,
-  //   offset: ["start end", "end end"],
-  // });
-
   const backgroundShiftY = useTransform(
     aboutParallaxScrollYProg,
-    [0, 1],
+    [0, 0.6],
     [0, 70]
   );
   const backgroundShiftX = useTransform(
     aboutParallaxScrollYProg,
-    [0, 1],
+    [0, 0.6],
     [0, -100]
   );
-  const textShift = useTransform(aboutParallaxScrollYProg, [0, 1], [0, -150]);
+  const sideLineShiftY = useTransform(
+    aboutParallaxScrollYProg,
+    [0, 0.6],
+    [0, 140]
+  );
+  const sideLineShiftX = useTransform(
+    aboutParallaxScrollYProg,
+    [0, 0.6],
+    [0, 100]
+  );
+  const bottomLineShiftY = useTransform(
+    aboutParallaxScrollYProg,
+    [0, 0.6],
+    [0, 100]
+  );
+  const bottomLineShiftX = useTransform(
+    aboutParallaxScrollYProg,
+    [0, 0.6],
+    [0, 200]
+  );
+  const textShift = useTransform(aboutParallaxScrollYProg, [0, 0.6], [0, -100]);
   const layoverShiftY = useTransform(
     aboutParallaxScrollYProg,
-    [0, 1],
-    [0, -170]
+    [0, 0.6],
+    [0, -200]
   );
-  const layoverShiftX = useTransform(aboutParallaxScrollYProg, [0, 1], [0, 50]);
-
-  console.log(textShift);
-
+  const layoverShiftX = useTransform(
+    aboutParallaxScrollYProg,
+    [0, 0.6],
+    [0, 50]
+  );
   const headingControls = useAnimation();
 
   const isInView = useInView(headingRef, { once: true });
@@ -84,56 +100,76 @@ const About = ({ aboutParallaxScrollYProg }: PropsType) => {
     <section
       ref={ref}
       id='about'
-      className='h-[130vh] flex justify-center overflow-hidden'
+      className='h-[150vh] md:h-[120vh] relative flex justify-center items-center overflow-hidden'
     >
-      <div className='w-full h-full flex flex-col items-center justify-center relative'>
-        <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-4xl w-1/2 md:w-3/4 h-1/3'>
-          <motion.div
-            className='h-full w-full bg-[#f5f5f5] shadow-xl'
-            style={{ y: backgroundShiftY, x: backgroundShiftX }}
-          ></motion.div>
-        </div>
+      <div className='w-full max-w-5xl h-fit lg:h-[450px] absolute'>
+        <div className='w-full h-full flex justify-center'>
+          <div className='absolute h-full max-w-4xl w-[90%] sm:w-3/4'>
+            <motion.div
+              className='h-[110%] sm:h-[115%] w-full bg-[#fafafa] shadow-xl relative'
+              style={{ y: backgroundShiftY, x: backgroundShiftX }}
+            >
+              <div className='absolute right-0 h-full'>
+                <motion.div
+                  className='h-full bg-[#222] w-[2px] '
+                  style={{ y: sideLineShiftY, x: sideLineShiftX }}
+                ></motion.div>
+              </div>
+              <div className='absolute bottom-0 w-full'>
+                <motion.div
+                  className='w-full bg-[#222] h-[2px] '
+                  style={{ y: bottomLineShiftY, x: bottomLineShiftX }}
+                ></motion.div>
+              </div>
+            </motion.div>
+          </div>
 
-        <motion.div
-          className='rounded-lg max-w-2xl w-1/2 p-4 relative mt-[150px]'
-          style={{ y: textShift }}
-        >
           <motion.div
-            style={{ y: layoverShiftY, x: layoverShiftX }}
-            className='bg-[#333] p-6 rounded-lg text-white absolute top-10 right-10'
+            className='rounded-lg lg:max-w-2xl max-w-[80%] sm:p-12 relative mt-[100px] mb-4'
+            style={{ y: textShift }}
           >
-            <h2>About me:</h2>
-          </motion.div>
-          <div
-            ref={headingRef}
-            className='overflow-hidden h-[60px] text-left mb-8 text-[#222]'
-          >
-            <motion.h2
-              variants={headerVariants}
+            <motion.div
+              style={{ y: layoverShiftY, x: layoverShiftX }}
+              className='bg-[#333] p-6 rounded-lg text-white absolute -top-28 max-sm:-left-5 sm:-top-16 sm:right-10'
+            >
+              <h2>About me.</h2>
+            </motion.div>
+            <div
+              ref={headingRef}
+              className='overflow-hidden h-[60px] text-left mb-8 text-[#222]'
+            >
+              <motion.h2
+                variants={headerVariants}
+                initial='initial'
+                animate={headingControls}
+                // style={{ y: headerShift, opacity: headerOpacity }}
+                className='text-3xl sm:text-4xl md:text-5xl mb-20'
+              >
+                Hi.
+              </motion.h2>
+            </div>
+            <motion.div
+              className='max-w-2xl text-left flex flex-col gap-4 text-base sm:text-lg md:text-xl'
+              variants={textVariants}
               initial='initial'
               animate={headingControls}
-              // style={{ y: headerShift, opacity: headerOpacity }}
-              className='text-5xl mb-20'
             >
-              Hi.
-            </motion.h2>
-          </div>
-          <motion.div
-            className='max-w-2xl text-left flex flex-col gap-4 text-xl'
-            variants={textVariants}
-            initial='initial'
-            animate={headingControls}
-          >
-            {/* <p>I'm Chris.</p> */}
-            <p>
-              I started learning programming and web development in my spare
-              time and never stopped expanding my knowledge within the field. I
-              am interested in clean and responsive front end design, web
-              accessibility, data management, and learning more about backends.
-            </p>
+              {/* <p>I'm Chris.</p> */}
+              <p>
+                Welcome to my digital space! I am a self-taught developer who
+                started learning programming and web development in my spare
+                time. Programming has given me a creative outlet while feeding
+                the more meticulous inclinations of my nature. I have grown
+                interested in clean and responsive front-end design, web
+                accessibility, data management, and delving further into back
+                ends and am always eager to learn a new skill or framework.
+                Check out my portfolio below to see what I'm capable of!
+              </p>
+            </motion.div>
           </motion.div>
-        </motion.div>
+        </div>
       </div>
+      {/* </div> */}
     </section>
   );
 };
