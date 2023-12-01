58c6f0dbde3a403bd8c937f74211d4879b58a29a
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-01 16:26:14 +0900

    Add font class

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
       padding: {
         hero: "80vh",
       },
