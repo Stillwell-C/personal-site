dd115d02783b87dfe03e2714196ab6955e5ad460
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-05 11:16:28 +0900

    Add different mobile layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -41,7 +41,8 @@ type PropsType = {
 
 const About = ({ aboutParallaxScrollYProg }: PropsType) => {
   const ref = useRef<HTMLDivElement>(null);
-  const headingRef = useRef<HTMLHeadingElement>(null);
+  const headingRef = useRef<HTMLDivElement>(null);
+  const mobileHeadingRef = useRef<HTMLDivElement>(null);
 
   const backgroundShiftY = useTransform(
     aboutParallaxScrollYProg,
@@ -84,6 +85,11 @@ const About = ({ aboutParallaxScrollYProg }: PropsType) => {
     [0, 0.6],
     [0, -200]
   );
+  const mobileLayoverShiftY = useTransform(
+    aboutParallaxScrollYProg,
+    [0, 0.6],
+    [0, -250]
+  );
   const layoverShiftX = useTransform(
     aboutParallaxScrollYProg,
     [0, 0.6],
@@ -92,79 +98,90 @@ const About = ({ aboutParallaxScrollYProg }: PropsType) => {
   const headingControls = useAnimation();
 
   const isInView = useInView(headingRef, { once: true });
+  const mobileIsInView = useInView(mobileHeadingRef, { once: true });
 
   useEffect(() => {
-    if (isInView) {
+    if (isInView || mobileIsInView) {
       headingControls.start("animate");
     }
-    console.log(isInView);
-  }, [isInView]);
+    console.log(isInView, mobileIsInView);
+  }, [isInView, mobileIsInView]);
+
+  const aboutText =
+    "Welcome to my digital space! I am a self-taught developer who started learning programming and web development in my spare time. I am always eager to learn a new skill or framework and have grown interested in clean and responsive front-end design, web accessibility, data management, and delving further into back ends. Check out my portfolio below to see what I'm capable of!";
+
+  const animatedText = (
+    <>
+      <div className='overflow-hidden h-[60px] text-left sm:mb-8 text-[#333]'>
+        <motion.h2
+          variants={headerVariants}
+          initial='initial'
+          animate={headingControls}
+          className='text-3xl sm:text-4xl md:text-5xl sm:mb-20'
+        >
+          Hi.
+        </motion.h2>
+      </div>
+      <motion.div
+        className='text-left flex flex-col gap-4 text-base sm:text-lg md:text-xl'
+        variants={textVariants}
+        initial='initial'
+        animate={headingControls}
+      >
+        <p>{aboutText}</p>
+      </motion.div>
+    </>
+  );
 
   return (
     <section
       ref={ref}
       id='about'
-      className='min-h-[150vh] md:min-h-[120vh] relative flex justify-center items-center overflow-hidden'
+      className='min-h-[80vh] sm:min-h-screen relative flex justify-center items-center overflow-hidden'
     >
-      <div className='w-full max-w-5xl flex justify-center py-20 sm:py-40'>
+      <div className='hidden sm:flex w-full justify-center py-20 sm:py-40'>
         <motion.div
-          className='max-w-[70%] md:max-w-[80%] lg:max-w-2xl px-6 pt-32 sm:pt-44 pb-8 relative bg-[#fafafa]'
+          className='max-w-[70%] lg:max-w-5xl w-3/4 px-6 pt-32 sm:pt-44 pb-8 relative bg-[#fafafa] shadow-xl'
           style={{ y: backgroundShiftY, x: backgroundShiftX }}
         >
-          <div className='absolute right-0 top-0 h-full'>
+          {/* <div className='absolute right-0 bottom-0 h-[97%]'>
             <motion.div
               className='h-full bg-[#222] w-[2px] '
               style={{ y: sideLineShiftY, x: sideLineShiftX }}
             ></motion.div>
           </div>
-          <div className='absolute left-0 bottom-0 w-full'>
+          <div className='absolute right-0 bottom-0 w-[99%]'>
             <motion.div
               className='w-full bg-[#222] h-[2px] '
               style={{ y: bottomLineShiftY, x: bottomLineShiftX }}
             ></motion.div>
-          </div>
+          </div> */}
           <motion.div
             style={{ y: layoverShiftY, x: layoverShiftX }}
             className='bg-[#333] p-6 rounded-lg text-white absolute -top-2 max-sm:-left-2 sm:top-10 sm:right-10'
           >
             <h2>About me.</h2>
           </motion.div>
-          <motion.div style={{ y: textShiftY, x: textShiftX }}>
-            <div
-              ref={headingRef}
-              className='overflow-hidden h-[60px] text-left mb-8 text-[#222]'
-            >
-              <motion.h2
-                variants={headerVariants}
-                initial='initial'
-                animate={headingControls}
-                // style={{ y: headerShift, opacity: headerOpacity }}
-                className='text-3xl sm:text-4xl md:text-5xl mb-20'
-              >
-                Hi.
-              </motion.h2>
-            </div>
-            <motion.div
-              className='max-w-2xl text-left flex flex-col gap-4 text-base sm:text-lg md:text-xl'
-              variants={textVariants}
-              initial='initial'
-              animate={headingControls}
-            >
-              {/* <p>I'm Chris.</p> */}
-              <p>
-                Welcome to my digital space! I am a self-taught developer who
-                started learning programming and web development in my spare
-                time. Programming has given me a creative outlet while feeding
-                the more meticulous inclinations of my nature. I have grown
-                interested in clean and responsive front-end design, web
-                accessibility, data management, and delving further into back
-                ends and am always eager to learn a new skill or framework.
-                Check out my portfolio below to see what I'm capable of!
-              </p>
-            </motion.div>
+          <motion.div
+            style={{ y: textShiftY, x: textShiftX }}
+            className='w-full px-4 pb-10'
+            ref={headingRef}
+          >
+            {animatedText}
           </motion.div>
         </motion.div>
       </div>
+      <motion.div
+        className='sm:hidden flex flex-col w-full px-6 py-28'
+        style={{ y: mobileLayoverShiftY }}
+      >
+        <div className='bg-[#333] p-6 rounded-lg text-white w-fit mb-8'>
+          <h2>About me.</h2>
+        </div>
+        <div className='flex flex-col' ref={mobileHeadingRef}>
+          {animatedText}
+        </div>
+      </motion.div>
     </section>
   );
 };
