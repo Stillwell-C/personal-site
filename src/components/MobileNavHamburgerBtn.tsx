45b284d9905c9990adff8e7295d8172eb910d0b2
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-08-16 11:25:42 +0900

    Add aria attributes to button

--- src/components/MobileNavHamburgerBtn.tsx
+++ src/components/MobileNavHamburgerBtn.tsx
@@ -17,6 +17,8 @@ const MobileNavHamburgerBtn = ({
     <button
       className='relative cursor-pointer w-8 h-8 z-10 pointer-events-auto'
       onClick={() => setOpen((prev: boolean) => !prev)}
+      aria-label='Navigation menu toggle'
+      aria-expanded={open}
     >
       <div
         className={`rounded h-1 w-7 top-4 absolute -mt-0.5 transition-all duration-700 before:content-[''] before:w-7 before:h-1 before:rounded before:absolute before:duration-700 before:transition-all before:-translate-x-3.5  after:w-7 after:rounded after:h-1 after:absolute after:duration-700 after:transition-all after:-translate-x-3.5 ${
